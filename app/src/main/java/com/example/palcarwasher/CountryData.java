4c2970dcb0767dbe1f021459cfbeb004b3396fad
shatha2098 <74787805+shatha2098@users.noreply.github.com>
2020-11-30 12:41:49 +0300

    add countryclass and sum code in register activity

---
+++ app/src/main/java/com/example/palcarwasher/CountryData.java
@@ -0,0 +1,19 @@
+package com.example.palcarwasher;
+
+public class CountryData {
+    public static final String[] countryNames =
+            {
+                    "Palestine","Jordan","Lebanon",
+                    "Syria","Saudi","Egypt", "Morocco",
+                    "Bahrain", "Iraq", "Algeria",
+                    "Tunisia","Qatar","UAE","Amman","Kuwait","Sudan", "Somalia"
+            };
+
+    public static final String[] countryAreaCodes =
+            {
+                    "970","962","961","963",
+                    "966","20","212","973",
+                    "964","213","216","974",
+                    "971","968","965","249","252"
+            };
+}
--- app/src/main/java/com/example/palcarwasher/register.java
+++ app/src/main/java/com/example/palcarwasher/register.java
@@ -3,12 +3,23 @@ package com.example.palcarwasher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class register extends AppCompatActivity {
 
+    private EditText PhoneNum;
+    private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+       // spinner=findViewById(R.id.spinnerCountries);
+        spinner.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,CountryData.countryNames));
+        PhoneNum = findViewById(R.id.phone_number);
+
+
     }
 }
