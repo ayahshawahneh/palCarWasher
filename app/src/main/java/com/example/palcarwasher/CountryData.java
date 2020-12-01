f744945823081722d88720d156a30c8cf26cb7fa
shatha2098 <74787805+shatha2098@users.noreply.github.com>
2020-12-01 14:01:33 +0300

    modify CountryData and reg

--- app/src/main/java/com/example/palcarwasher/CountryData.java
+++ app/src/main/java/com/example/palcarwasher/CountryData.java
@@ -3,7 +3,7 @@ package com.example.palcarwasher;
 public class CountryData {
     public static final String[] countryNames =
             {
-                    "Palestine 972","Jordan 962","Lebanon 961",
+                    "Palestine 970","Jordan 962","Lebanon 961",
                     "Syria 963","Saudi 966","Egypt 20", "Morocco 212",
                     "Bahrain 973", "Iraq 964", "Algeria 213",
                     "Tunisia 216","Qatar 974","UAE 971","Amman 968",
@@ -12,7 +12,7 @@ public class CountryData {
 
     public static final String[] countryAreaCodes =
             {
-                    "972","962","961","963",
+                    "970","962","961","963",
                     "966","20","212","973",
                     "964","213","216","974",
                     "971","968","965","249","252"
